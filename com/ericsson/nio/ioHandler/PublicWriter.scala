1064e8cc8fdcb12ff14405426bab1769262a2382
U-EAPAC\esipeng <esipeng@E768B599FDE410.ericsson.se>
2014-02-28 13:25:12 +0800

    optimized

--- com/ericsson/nio/ioHandler/PublicWriter.scala
+++ com/ericsson/nio/ioHandler/PublicWriter.scala
@@ -6,14 +6,14 @@ import java.nio.ByteBuffer
 
 class PublicWriter() extends BaseIOHandler() {
     var disconnectAfterSent:Boolean = false;
-	val sendQueue = new ConcurrentLinkedQueue[ByteBuffer];
-	var currentContent:ByteBuffer = null;//current sending buffer
+	//val sendQueue = new ConcurrentLinkedQueue[ByteBuffer];
+	var content:ByteBuffer = null;//current sending buffer
 	
 	def this(tsk:SelectionKey) = {this();sk=tsk;disconnectAfterSent=true}
 	def this(tsk:SelectionKey,td:Boolean) = {this();sk=tsk;disconnectAfterSent=td}
 	def run{	
 	  //check if current content sending is completed or not
-	  var content:ByteBuffer = null;
+	  /*var content:ByteBuffer = null;
 	  if(currentContent != null && currentContent.hasRemaining())
 	    content = currentContent;
 	  else if(sendQueue.isEmpty()){ //nothing to send
@@ -24,25 +24,25 @@ class PublicWriter() extends BaseIOHandler() {
 	    //there is something to send
 	    currentContent = sendQueue.remove();
 	    content = currentContent;
-	  }
+	  }*/
 	  
 	  if(content != null && content.hasRemaining())	{
 		 try{
 		   var len = sc.write(content);
 		   //println("writing " + len + " bytes , content has " + content.remaining() + " remaining.");
 		   if(len < 0)	{ //fault has find in sent
-		     println("disconnect after send fault");
+		     //println("disconnect after send fault");
 	         disconnect;
 		   }
 	       else {
-	         if(content.hasRemaining() == false && sendQueue.size() == 0) {//nothing left to send
+	         if(content.hasRemaining() == false) {//nothing left to send
 		         if(disconnectAfterSent)	{
 		           //println("disconnect after sent");
 		           disconnect;
 		         }
 		         else	{
 		           //println("chagnes to reading mode ");
-		           currentContent = null;
+		           content = null;
 		           sk.interestOps(SelectionKey.OP_READ);
 		           sk.selector().wakeup();
 		         }
@@ -61,7 +61,7 @@ class PublicWriter() extends BaseIOHandler() {
 	       disconnect;
 	     }
 	     else	{
-	       currentContent = null;
+	       content = null;
 	       sk.interestOps(SelectionKey.OP_READ);
 	       sk.selector().wakeup();
 	    }
@@ -69,11 +69,13 @@ class PublicWriter() extends BaseIOHandler() {
 	}
 	
 	def sendData(data:ByteBuffer)	{
-	  val copiedData = ByteBuffer.allocate(data.remaining());
+	  //val copiedData = ByteBuffer.allocate(data.remaining());
 	  //println("sending data : " + data.remaining());
-	  copiedData.put(data);
-	  copiedData.flip();
-	  sendQueue.add(copiedData);
+	  //copiedData.put(data);
+	  //copiedData.flip();
+	  //sendQueue.add(data);
+	  content = data;
+	  //println("new");
 	  sk.interestOps(SelectionKey.OP_WRITE);
 	  sk.selector().wakeup();
 	}
